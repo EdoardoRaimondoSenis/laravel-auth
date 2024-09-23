250427654dff92d6eb57d31504d89f1d3f80749c
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-09-23 17:51:18 +0200

    Migration e seeder creati, dati con il faker generati nel database

---
+++ resources/views/admin/partials/nav.blade.php
@@ -0,0 +1,9 @@
+<aside class="text-bg-dark">
+
+    <ul>
+        <li><a href=""></a>Home</li>
+        <li><a href=""></a>Elenco Post</li>
+        <li><a href=""></a>Nuovo post</li>
+    </ul>
+
+</aside>
--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,1 +1,5 @@
 @extends('layouts.guest')
+
+@section('content')
+    <h1>Pagina Pubblica</h1>
+@endsection
